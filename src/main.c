7f41038a414fd70d7c9c5fd4b3cd4fda4daf4581
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-08-05 02:18:21 +0900

    コンパイル出力ファイルのパスおよび名前を明示的にしていできるように変更
    コマンドラインでのファイル指定を、以下の方法で行えるように変更:
    
    ./osecpubasic BASICファイル.bas
    この場合は、同名で拡張子が .ask に置き換わったファイルとして、
    カレントディレクトリ内へコンパイル結果が出力される。
    
    ./osecpubasic BASICファイル.bas 出力ファイル名.ask
    この場合は、出力ファイル名を明示的に指定できる。
    また、出力ファイル名にはパスも含めることも可能。（カレントディレクトリ以外への出力も指定できる）

--- src/main.c
+++ src/main.c
@@ -20,15 +20,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "config.h"
+
+extern FILE* yyin;
+extern FILE* yyout;
+extern FILE* yyaskA;
 
 static void print_usage(void)
 {
-        printf("使用法: %s [入力ファイル]\n"
+        printf("使用法: %s 入力ファイル.bas [出力ファイル.ask]\n"
                "\n"
                "%s version %s\n"
                "Copyright(C) 2013 Takeutch Kemeco\n"
                "GNU General Public License version 2\n"
+               "This is free software; see the source for copying conditions.  There is NO\n"
+               "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
+               "[参考訳 -- 法的効力は英文が適用されます]\n"
+               "これはフリーソフトウェアです -- 複製についての条件はソースを見ましょう。\n"
+               "一切の保証はありません -- 商業性や目的適合性についての保証すらありません。\n"
                "\n"
                "repository: <https://github.com/takeutch-kemeco/osecpu-basic>\n"
                "bug report: <%s>\n",
@@ -37,21 +47,16 @@ static void print_usage(void)
                PACKAGE_BUGREPORT);
 }
 
-static void print_file_open_err(char* s)
+static void print_file_open_err(const char* s)
 {
         printf("ファイル %s のオープンでエラーが発生しました。中止します。\n", s);
 }
 
-static FILE* open_in_file(int argc, char** argv)
+static FILE* open_in_file(const char* in_path)
 {
-        if (argc == 1) {
-                print_usage();
-                exit(EXIT_FAILURE);
-        }
-
-        FILE* fp = fopen(argv[1], "rt");
+        FILE* fp = fopen(in_path, "rt");
         if (fp == NULL) {
-                print_file_open_err(argv[1]);
+                print_file_open_err(in_path);
                 print_usage();
                 exit(EXIT_FAILURE);
         }
@@ -71,16 +76,11 @@ static FILE* open_null_out_file(void)
         return fp;
 }
 
-static FILE* open_out_file(int argc, char** argv)
+static FILE* open_out_file(const char* out_path)
 {
-        char* bas_name = argv[1];
-        char ask_name[0x1000];
-        strcpy(ask_name, bas_name);
-        strcat(ask_name, ".ask");
-
-        FILE* fp = fopen(ask_name, "wt");
+        FILE* fp = fopen(out_path, "wt");
         if (fp == NULL) {
-                print_file_open_err(ask_name);
+                print_file_open_err(out_path);
                 print_usage();
                 exit(EXIT_FAILURE);
         }
@@ -88,15 +88,54 @@ static FILE* open_out_file(int argc, char** argv)
         return fp;
 }
 
-extern FILE* yyin;
-extern FILE* yyout;
-extern FILE* yyaskA;
+static int path_to_filename(char* dst, char* src)
+{
+        char* p = src;
+        char* head = src;
+
+        while (*p != '\0') {
+                if (*p++ == '/')
+                        head = p;
+        }
+
+        strcpy(dst, head);
+}
+
+static int swap_filename_extention_bas_to_ask(char* a)
+{
+        size_t len = strlen(a);
+        if (strcat(a + len - 3, "bas") == 0) {
+                printf("読み込もうとしてるソースファイルの拡張子が bas ではありません\n");
+                print_usage();
+                exit(EXIT_FAILURE);
+        }
+
+        strcpy(a + len - 3, "ask");
+}
 
 int main(int argc, char** argv)
 {
-        yyin = open_in_file(argc, argv);
+        char* in_path = argv[1];
+        char out_path[0x1000];
+
+        switch (argc) {
+        case 2:
+                path_to_filename(out_path, in_path);
+                swap_filename_extention_bas_to_ask(out_path);
+                break;
+
+        case 3:
+                strcpy(out_path, argv[2]);
+                break;
+
+        default:
+                print_usage();
+                exit(EXIT_FAILURE);
+        }
+
+        yyin = open_in_file(in_path);
         yyout = open_null_out_file();
-        yyaskA = open_out_file(argc, argv);
+        yyaskA = open_out_file(out_path);
 
         start_pre_process();
         while (yylex() != 0) {
